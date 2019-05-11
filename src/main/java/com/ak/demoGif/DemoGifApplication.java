4391adb4e3d2470d0f9063d0b26d1d997d7e0a24
michalqq <kraciukmichal@gmail.com>
2019-05-11 15:38:09 +0200

    feature commit

--- src/main/java/com/ak/demoGif/DemoGifApplication.java
+++ src/main/java/com/ak/demoGif/DemoGifApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoGifApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoGifApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoGifApplication.class, args);
+    }
 
 }
